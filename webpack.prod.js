33ad2d333f961ad0cea6803943b54f197344b5de
khaled912 <khaled22es@gmail.com>
2021-09-15 23:08:03 +0200

    setup webpack prod configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,9 +3,8 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
